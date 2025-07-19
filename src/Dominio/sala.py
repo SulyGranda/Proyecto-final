cc6325c9d0d32a283100b80546d3556cfb85fce9
SulyGranda <grandasuly@gmail.com>
2025-07-19 03:16:29 -0500

    Update sala.py

--- src/Dominio/sala.py
+++ src/Dominio/sala.py
@@ -1,1 +1,17 @@
+class Sala:
+    def __init__(self, id_sala, nombre, capacidad):
+        self._id = id_sala
+        self._nombre = nombre
+        self._capacidad = capacidad
 
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def nombre(self):
+        return self._nombre
+
+    @property
+    def capacidad(self):
+        return self._capacidad
